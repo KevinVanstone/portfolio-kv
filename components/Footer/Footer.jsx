03b8b7d4f366d287dd3a69daf0b401d707a73301
KevinVanstone <40321708+KevinVanstone@users.noreply.github.com>
2023-08-06 16:02:43 -0700

    Footer - initial MVP layout

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styles from "./Footer.module.scss";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const Footer = () => {
   return (
     <section className={styles[`footer`]}>
-      <h3 className={styles[`subhed`]}>This is the footer!</h3>
+      <div className={styles[`footer__logoList`]}>
+        <Link href="mailto:kv@theflyingv.com">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            style={{ fontSize: 40, color: "white", marginLeft: "5px" }}
+          />
+        </Link>
+        <Link href="https://github.com/KevinVanstone">
+          <FontAwesomeIcon
+            icon={faGithub}
+            style={{ fontSize: 40, color: "white", marginLeft: "5px" }}
+          />
+        </Link>
+        <Link href="https://www.linkedin.com/in/kevin-vanstone">
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            style={{ fontSize: 40, color: "white", marginLeft: "5px" }}
+          />
+        </Link>
+      </div>
+      <h5 className={styles[`subhed`]}>Created by Kevin Vanstone</h5>
     </section>
   );
 };
