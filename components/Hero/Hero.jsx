bd0861a2f69a5b68d227a5eda26e1b594467dbcc
KevinVanstone <40321708+KevinVanstone@users.noreply.github.com>
2023-08-06 16:46:39 -0700

    Hero - bg and basic text, styling tbd

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,8 +4,8 @@ import styles from "./Hero.module.scss";
 const Hero = () => {
   return (
     <section className={styles[`hero`]}>
-      <h1 className={styles[`hed`]}>Kevin Vanstone</h1>
-      <h2 className={styles[`subhed`]}>
+      <h1 className={styles[`hero__hed`]}>Kevin Vanstone</h1>
+      <h2 className={styles[`hero__subhed`]}>
         Web Developer, Vancouver British Columbia
       </h2>
     </section>
