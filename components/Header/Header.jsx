e5f5762a79fca43f83b7f9d80587a4fa24a25635
KevinVanstone <40321708+KevinVanstone@users.noreply.github.com>
2023-08-06 16:31:25 -0700

    Header - mobile MVP

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import Image from "next/image";
+import headshot from "../../assets/images/headshot.jpeg";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+const headshotStyle = {
+  borderRadius: "50%",
+};
 
 const Header = () => {
   return (
     <section className={styles[`header`]}>
-      <h3 className={styles[`subhed`]}>This is the header!</h3>
+      <Image
+        src={headshot}
+        width={60}
+        height={60}
+        alt="Developer Kevin Vanstone"
+        style={headshotStyle}
+      />
+      <FontAwesomeIcon
+        icon={faBars}
+        style={{ fontSize: 40, color: "white", marginLeft: "5px" }}
+      />
     </section>
   );
 };
